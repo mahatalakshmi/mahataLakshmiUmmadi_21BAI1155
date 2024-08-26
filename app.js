fb4e1e94b695351b3a6d23aca13869d5e4916f0b
Mahata <mahata2657@gmail.com>
2024-08-26 19:22:51 +0530

    hitwicket chess assignmnet upload

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+app.use(express.static('public'));
+var http = require('http').Server(app);
+var port = process.env.PORT || 5000;
+app.get('/', function(req, res) {
+res.sendFile(__dirname + '/public/index.html');
+});
+
+http.listen(port, function() {
+console.log('listening on : ' + port);
+});
+var io = require('socket.io')(http);
+io.on('connection', function(socket) {
+    console.log('new connection');
+    socket.on('move', function(msg) {
+    socket.broadcast.emit('move', msg);
+    });
+    });
